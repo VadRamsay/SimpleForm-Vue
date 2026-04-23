1db9d3c6d128d223c4d62c544663c7d6ebc48991
VadRamsay <VadimRamsay@gmail.com>
2026-04-23 12:40:07 -0500

    Minor Tweaks

--- src/SimpleForm.vue
+++ src/SimpleForm.vue
@@ -9,9 +9,9 @@
         <input class="text-center text-blue-700 border m-2 border-gray-300 rounded p-2" type="text" placeholder="Name Please" />
         <input class="text-center text-blue-700 border m-2 border-gray-300 rounded p-2" type="email" placeholder="Email Please" />
         <input class="text-center text-blue-700 border m-2 pb-46 border-gray-300 rounded p-2" type="text" placeholder="Just Some Random Text..." />
-        <button class="border-2 border-gray-500 m-2 rounded p-2" type="submit">Send</button>
+        <button class="border-2 border-gray-500 hover:bg-green-100 m-2 rounded p-2" type="submit">Send</button>
       </form>
-      
+
     </div>
   </div>
 </template>
