59c4e03b76a19ff50faac2760666b4989305afb4
VadRamsay <VadimRamsay@gmail.com>
2026-04-23 10:20:40 -0500

    Form Creation

--- src/SimpleForm.vue
+++ src/SimpleForm.vue
@@ -1,13 +1,15 @@
 
 <template>
-    <div>
-        <h2>Contact Form</h2>
-        <form>
-            <input type="text" placeholder="Name Please" />
-            <input type="email" placeholder="Email Please" />
-            <button type="submit">Send</button>
-        </form>
+  <div class="min-h-screen flex items-center justify-center">
+    <div class="bg-gray-200 p-8 flex flex-col gap-2">
+      <h2 class="text-center">Contact Form</h2>
+      <form class="flex flex-col w-96">
+        <input class="text-center border m-2 border-gray-300 rounded p-2" type="text" placeholder="Name Please" />
+        <input class="text-center border m-2 border-gray-300 rounded p-2" type="email" placeholder="Email Please" />
+        <button type="submit">Send</button>
+      </form>
     </div>
+  </div>
 </template>
 
 <script setup>
